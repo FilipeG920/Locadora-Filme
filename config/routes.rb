c27653a58769cb451364f8d716416b31675f0319
FilipeG920 <genesisfilipe11@gmail.com>
2025-11-05 12:43:50 -0300

    feat: Criar controller do devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
+  devise_for :controllers
   resources :emprestimos
   resources :copia_filmes
   resources :filmes
   resources :generos
-  devise_for :clientes
+  devise_for :clientes, controllers: {
+    registrations: "clientes/registrations",
+    sessions: "clientes/sessions"
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  devise_scope :cliente do
+    root to: "devise/registrations#new"
+  end
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
