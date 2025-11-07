651006cc670b4df6f8263e6fbf4ab60ddde511f1
FilipeG920 <genesisfilipe11@gmail.com>
2025-11-06 22:27:10 -0300

    Reorganizar código

--- config/routes.rb
+++ config/routes.rb
@@ -25,11 +25,13 @@ Rails.application.routes.draw do
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  devise_scope :cliente do
-    root to: "devise/registrations#new"
-  end
+  # devise_scope :cliente do
+  #  root to: "devise/registrations#new"
+  # end
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+
   get "up" => "rails/health#show", as: :rails_health_check
   root "home#index"
 
